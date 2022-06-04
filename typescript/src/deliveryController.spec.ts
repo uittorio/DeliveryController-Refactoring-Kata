c58bb473f715bbe7c16af0b7805d7c8d33e6bf79
vittorioguerriero <vittorio.guerriero@cazoo.co.uk>
2022-06-04 22:41:11 +0100

    refactor the test to create delivery and delivery events. this improves the readibility of the tests

--- typescript/src/deliveryController.spec.ts
+++ typescript/src/deliveryController.spec.ts
@@ -1,27 +1,18 @@
-import { Delivery, DeliveryController } from './deliveryController';
+import { Delivery, DeliveryController, DeliveryEvent } from './deliveryController';
 import { FakeEmailService } from '../fake/fakeEmailService';
 import { FakeSmsService } from '../fake/fakeSmsService';
 
 describe('When the customer want to receive updates by email', () => {
     describe('When an existing delivery is updated', () => {
         it('should send an email to the contact email', () => {
-            const delivery: Delivery = {
-                arrived: false,
-                contactEmail: 'vittorio.gue@gmail.com',
+            const delivery: Delivery = createDelivery({
                 id: 'existing-delivery-for-vittorio',
-                location: undefined,
-                onTime: false,
-                timeOfDelivery: new Date()
-            }
+                contactEmail: 'vittorio.gue@gmail.com'
+            });
             const fakeEmailService = new FakeEmailService();
-            new DeliveryController([delivery], fakeEmailService).updateDelivery({
+            new DeliveryController([delivery], fakeEmailService).updateDelivery(createDeliveryEvent({
                 id: 'existing-delivery-for-vittorio',
-                location: {
-                    latitude: 120,
-                    longitude: 250
-                },
-                timeOfDelivery: new Date()
-            })
+            }))
 
             expect(fakeEmailService.emailsSent).toStrictEqual([{
                 contactEmail: 'vittorio.gue@gmail.com',
@@ -31,24 +22,14 @@ describe('When the customer want to receive updates by email', () => {
     });
     describe('When a non existing delivery is updated', () => {
         it('should not send an email', () => {
-            const delivery: Delivery = {
-                arrived: false,
-                contactEmail: 'vittorio.gue@gmail.com',
-                id: 'unknown-delivery',
-                location: undefined,
-                onTime: false,
-                timeOfDelivery: new Date()
-            }
+            const delivery: Delivery = createDelivery({
+                id: 'existing-delivery-for-vittorio',
+            });
 
             const fakeEmailService = new FakeEmailService();
-            new DeliveryController([delivery], fakeEmailService).updateDelivery({
-                id: 'existing-delivery-for-vittorio',
-                location: {
-                    latitude: 120,
-                    longitude: 250
-                },
-                timeOfDelivery: new Date()
-            })
+            new DeliveryController([delivery], fakeEmailService).updateDelivery(createDeliveryEvent({
+                id: 'unknown-delivery',
+            }))
 
             expect(fakeEmailService.emailsSent).toEqual([])
         });
@@ -56,38 +37,20 @@ describe('When the customer want to receive updates by email', () => {
 
     describe('When there is another delivery for a customer', () => {
         it('should send an email to notify the customer that the deliver will arrive soon', async () => {
-            const delivery: Delivery = {
-                arrived: false,
-                contactEmail: 'vittorio.gue@gmail.com',
+            const delivery: Delivery = createDelivery(createDelivery({
                 id: 'existing-delivery-for-vittorio',
-                location: undefined,
-                onTime: false,
-                timeOfDelivery: new Date()
-            }
-
-            const anotherDelivery: Delivery = {
-                arrived: false,
                 contactEmail: 'vittorio.gue@gmail.com',
+            }));
+
+            const anotherDelivery: Delivery = createDelivery({
                 id: 'another-delivery-for-vittorio',
-                location: {
-                    latitude: 200,
-                    longitude: 150
-                },
-                onTime: false,
-                timeOfDelivery: new Date()
-            }
+                contactEmail: 'vittorio.gue@gmail.com',
+            });
             const fakeEmailService = new FakeEmailService()
 
             const deliveryController = new DeliveryController([delivery, anotherDelivery], fakeEmailService);
 
-            await deliveryController.updateDelivery({
-                id: 'existing-delivery-for-vittorio',
-                location: {
-                    latitude: 120,
-                    longitude: 250
-                },
-                timeOfDelivery: new Date()
-            })
+            await deliveryController.updateDelivery(createDeliveryEvent())
 
             expect(fakeEmailService.emailsSent.length).toBe(2);
             expect(fakeEmailService.emailsSent).toStrictEqual([{
@@ -104,24 +67,16 @@ describe('When the customer want to receive updates by email', () => {
 describe('When the customer wants to receive updates by sms', () => {
     describe('When an existing delivery is updated', () => {
         it('should send an email to the customer phone number', () => {
-            const delivery: Delivery = {
-                arrived: false,
-                contactEmail: 'vittorio.gue@gmail.com',
+            const delivery = createDelivery();
+            createDelivery({
+                id: 'existing-delivery-for-vittorio',
                 contactPhoneNumber: '07777777777',
-                id: 'existing-delivery-for-vittorio',
-                location: undefined,
-                onTime: false,
-                timeOfDelivery: new Date()
-            }
-            const fakeSmsService = new FakeSmsService();
-            new DeliveryController([delivery], fakeSmsService).updateDelivery({
-                id: 'existing-delivery-for-vittorio',
-                location: {
-                    latitude: 120,
-                    longitude: 250
-                },
-                timeOfDelivery: new Date()
+                contactEmail: 'vittorio.gue@gmail.com',
             })
+            const fakeSmsService = new FakeSmsService();
+            new DeliveryController([delivery], fakeSmsService).updateDelivery(createDeliveryEvent({
+                id: 'existing-delivery-for-vittorio',
+            }))
 
             expect(fakeSmsService.smsSent).toStrictEqual({
                 contactNumber: '07777777777'
@@ -129,3 +84,31 @@ describe('When the customer wants to receive updates by sms', () => {
         });
     });
 });
+
+function createDeliveryEvent(deliveryEvent?: Partial<DeliveryEvent>): DeliveryEvent {
+    return {
+        id: 'existing-delivery-for-vittorio',
+        location: {
+            latitude: 120,
+            longitude: 250
+        },
+        timeOfDelivery: new Date(),
+        ...deliveryEvent
+    };
+}
+
+function createDelivery(delivery?: Partial<Delivery>): Delivery {
+    return {
+        arrived: false,
+        contactEmail: 'vittorio.gue@gmail.com',
+        contactPhoneNumber: '07777777777',
+        id: 'existing-delivery-for-vittorio',
+        location: {
+            latitude: 120,
+            longitude: 200
+        },
+        onTime: false,
+        timeOfDelivery: new Date(),
+        ...delivery
+    };
+}
